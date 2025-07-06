76ad9cb0557b728d99482260bb6fa3fb8e1bbd6f
Dev-Will <williampinheiro778@gmail.com>
2025-07-05 21:14:41 -0400

    Delete src directory

--- src/entities/enemy.js
+++
@@ -1,7 +0,0 @@
-// test!
-
-export let inimigos = [
-  { x: 500, y: 384 - 29, largura: 39, altura: 28, velocidade: 3, vivo: true },
-  { x: 700, y: 384 - 29, largura: 39, altura: 28, velocidade: 3, vivo: true },
-  { x: 900, y: 384 - 29, largura: 39, altura: 28, velocidade: 3, vivo: true }
-];
--- src/entities/ring.js
+++
@@ -1,8 +0,0 @@
-// test!
-
-export let rings = [
-  { x: 729, y: 360, coletada: false },
-  { x: 599, y: 360, coletada: false },
-  { x: 469, y: 360, coletada: false },
-  { x: 339, y: 360, coletada: false }
-];
