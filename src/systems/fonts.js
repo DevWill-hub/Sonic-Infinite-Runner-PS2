71ed6028956be983bdf0bc89c20f5523462e7873
Dev-Will <williampinheiro778@gmail.com>
2025-09-04 21:09:52 -0400

    Delete src directory

--- src/systems/fonts.js
+++
@@ -1,7 +0,0 @@
-export const fontScala1 = new Font("assets/fonts/mania.ttf");
-fontScala1.color = Color.new(255, 255, 255);
-fontScala1.scale = 1.0;
-
-export const fontScala2 = new Font("assets/fonts/mania.ttf");
-fontScala2.color = Color.new(255, 255, 255);
-fontScala2.scale = 0.5;
